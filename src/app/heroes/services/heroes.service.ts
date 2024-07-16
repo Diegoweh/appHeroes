b0f7ffd3b0b991ceb4b4c56e062a4854b9f23eca
Diego Celaya <diegocelajah@gmail.com>
2024-07-16 11:14:56 -0700

    se completo el authentificate

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { environments } from '../../../environments/environments';
 import { Hero } from '../interfaces/hero.interface';
 
@@ -24,6 +24,26 @@ export class HeroesService {
 
   getSuggestions( query: string ): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
+  }
+
+  addHero( hero: Hero ): Observable<Hero> {
+    const heroWithoutId = {...hero, id: undefined}
+    return this.http.post<Hero>(`${ this.baseUrl }/heroes`, heroWithoutId );
+  }
+
+  updateHero( hero: Hero ): Observable<Hero> {
+    if ( !hero.id ) throw Error('Hero id is required');
+
+    return this.http.patch<Hero>(`${ this.baseUrl }/heroes/${ hero.id }`, hero );
+  }
+
+  deleteHeroById( id: string ): Observable<boolean> {
+
+    return this.http.delete(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        map( resp => true ),
+        catchError( err => of(false) ),
+      );
   }
 
 }
